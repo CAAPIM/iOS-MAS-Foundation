d7854890769bc8334d9b576409048095593f952c
James Go <hun.go@ca.com>
2016-06-06 09:34:57 -0700

    MCT-278 iOS: Interface Alignment (continued) - change the header

--- MASFoundation/Classes/MASProximityLoginDelegate.h
+++ MASFoundation/Classes/MASProximityLoginDelegate.h
@@ -2,9 +2,12 @@
 //  MASProximityLoginDelegate.h
 //  MASFoundation
 //
-//  Created by Hun Go on 2016-06-03.
-//  Copyright © 2016 CA Technologies. All rights reserved.
+//  Copyright (c) 2016 CA, Inc.
 //
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
 @import Foundation;
 
 #import "MASConstants.h"
