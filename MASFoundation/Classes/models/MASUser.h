da62f6523ccea51122951d3edf9862d282ccb8fe
James Go <hun.go@ca.com>
2016-10-27 17:13:38 -0700

    US246928 - Local Authentication implementation : adding missing documentation

--- MASFoundation/Classes/models/MASUser.h
+++ MASFoundation/Classes/models/MASUser.h
@@ -206,6 +206,15 @@
 
 
 
+/**
+ Authenticate a user via asynchronous request with authorization code.
+ 
+ This will create an [MASUser currentUser] upon a successful result.
+
+ @param authorizationCode The authorization code for the user.
+ @param completion        The MASCompletionErrorBlock block that receives the results.  On a successful completion, the user
+ *  available via [MASUser currentUser] has been updated with the new information.
+ */
 + (void)loginWithAuthorizationCode:(NSString *)authorizationCode completion:(MASCompletionErrorBlock)completion;
 
 
