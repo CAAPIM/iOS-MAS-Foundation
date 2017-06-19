d1ba5fa8b9a60ffbea69a4d6c277e5a8592ace8f
James Go <hun.go@ca.com>
2017-06-19 10:33:46 -0700

    US349497 : [iOS] Implement new authentication design - header copyright

--- MASFoundation/Classes/models/AuthCredentials/MASAuthCredentialsAuthorizationCode.h
+++ MASFoundation/Classes/models/AuthCredentials/MASAuthCredentialsAuthorizationCode.h
@@ -2,8 +2,10 @@
 //  MASAuthCredentialsAuthorizationCode.h
 //  MASFoundation
 //
-//  Created by Hun Go on 2017-05-31.
-//  Copyright © 2017 CA Technologies. All rights reserved.
+//  Copyright (c) 2016 CA. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import "MASAuthCredentials.h"
--- MASFoundation/Classes/models/MASClaims.m
+++ MASFoundation/Classes/models/MASClaims.m
@@ -2,8 +2,10 @@
 //  MASClaims.m
 //  MASFoundation
 //
-//  Created by Hun Go on 2017-04-04.
-//  Copyright © 2017 CA Technologies. All rights reserved.
+//  Copyright (c) 2016 CA. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import "MASClaims.h"
