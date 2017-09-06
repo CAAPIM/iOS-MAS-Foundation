9e165a2f9b82686c4b6f1b9981742a0e556ba080
Rodrigo Reis <rodrigo.reis@ca.com>
2017-09-06 15:31:43 -0700

    Updated license information in the files.

--- MASFoundation/Classes/_private_/models/Network/MASRequest+MASPrivate.m
+++ MASFoundation/Classes/_private_/models/Network/MASRequest+MASPrivate.m
@@ -2,8 +2,10 @@
 //  MASRequest+MASPrivate.m
 //  MASFoundation
 //
-//  Created by Reis, Rodrigo on 2017-08-31.
-//  Copyright © 2017 CA Technologies. All rights reserved.
+//  Copyright (c) 2017 CA. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import <MASFoundation/MASFoundation.h>
--- MASFoundation/Classes/models/Network/MASRequestBuilder.m
+++ MASFoundation/Classes/models/Network/MASRequestBuilder.m
@@ -2,8 +2,10 @@
 //  MASRequestBuilder.m
 //  MASFoundation
 //
-//  Created by Reis, Rodrigo on 2017-08-29.
-//  Copyright © 2017 CA Technologies. All rights reserved.
+//  Copyright (c) 2017 CA. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import "MASRequestBuilder.h"
