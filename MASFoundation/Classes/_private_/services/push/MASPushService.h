3a8f95e70c7afd94c4d9a0a0ac785edaa9018540
Rodrigo Reis <rodrigo.reis@ca.com>
2017-11-09 14:28:56 -0800

    US373049 : [iOS] Register for Push Notification SDK API

--- MASFoundation/Classes/_private_/services/push/MASPushService.h
+++ MASFoundation/Classes/_private_/services/push/MASPushService.h
@@ -2,8 +2,10 @@
 //  MASPushService.h
 //  MASFoundation
 //
-//  Created by Reis, Rodrigo on 2017-11-06.
-//  Copyright © 2017 CA Technologies. All rights reserved.
+//  Copyright (c) 2017 CA. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import <MASFoundation/MASFoundation.h>
