fcbc15d15320aa69db4f496e07a20d64ab18f45c
James Go <hun.go@ca.com>
2018-04-27 12:30:58 -0700

    DE361046 : [iOS][MASFoundation] MAS initialization method does not complete if user does not decide on any system prompts

--- MASFoundation/Classes/_private_/services/location/MASLocationService.m
+++ MASFoundation/Classes/_private_/services/location/MASLocationService.m
@@ -288,7 +288,7 @@
     MASINTULocationManager *locMgr = [MASINTULocationManager sharedInstance];
     
     return [locMgr requestLocationWithDesiredAccuracy:MASINTULocationAccuracyNeighborhood
-        timeout:5
+        timeout:0.1
         delayUntilAuthorized:NO
         block:^(CLLocation *currentLocation, MASINTULocationAccuracy achievedAccuracy, MASINTULocationStatus status)
         {
